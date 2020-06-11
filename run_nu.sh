2e748645f5770865b47f35f5f81ec271010f61cb
Tanaz Mohayai <tmohayai@users.noreply.github.com>
2020-06-11 14:05:17 -0500

    gevgen executible for generating GENIE GHEPs

---
+++ run_nu.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+VOLUME="volMPD"
+GDML_SHORT="nd_hall_only_mpd_antifid"
+GEO="/pnfs/dune/persistent/users/mtanaz/test_8/${GDML_SHORT}.gdml"
+MODE="neutrino"
+POT=7.5e13
+NEVENTS=100
+ProcId=$1
+LOC="DUNE_ND_HALL"
+FLUXFILES="/pnfs/dune/persistent/users/mtanaz/gsimple_subdetectors/neutrino/gsimple*.root"
+FINAL_DEST="/pnfs/dune/persistent/users/mtanaz/test_8/production"
+
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+
+setup genie        v2_12_10c  -q e15:prof
+setup genie_xsec   v2_12_10   -q DefaultPlusValenciaMEC
+setup genie_phyopt v2_12_10   -q dkcharmtau
+setup dk2nu        v01_05_01b -q e15:prof
+setup ifdhc
+
+#########=========================================
+seed=${RANDOM}
+
+#echo the path to the directory where your job runs, in case
+#you want to recover files
+echo "========================================================================="
+echo "HOST=${HOST}, seed=${seed}"
+echo "Running genie generation for volume ${VOLUME} with geometry ${GEO} in mode ${MODE}"
+echo "Location ${LOC}, POT=${POT} or NEVENTS=${NEVENTS}"
+echo "Flux files: ${FLUXFILES}"
+echo "========================================================================="
+echo " "
+echo " "
+
+# Create the ray flux file ${GDML_SHORT}.${VOLUME}.maxpl.xml
+
+echo "Generation of the ray flux file ${GDML_SHORT}.${VOLUME}.maxpl.xml"
+
+gevgen_fnal \
+-f ${FLUXFILES},${LOC} \
+-g ${GEO} \
+-t ${VOLUME} \
+-m "+${GDML_SHORT}.${VOLUME}.maxpl.xml" \
+-S "+25000" \
+-L "cm" -D "g_cm3" \
+-n ${NEVENTS} \
+--seed ${seed} \
+-r 9999 \
+-o ${MODE} \
+--message-thresholds Messenger_production.xml \
+--cross-sections ${GENIEXSECPATH}/gxspl-FNALsmall.xml \
+--event-generator-list Default+CCMEC
+
