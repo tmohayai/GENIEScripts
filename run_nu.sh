c92c906db5054eb0cc4d9816e4a9991b7cf138f8
Tanaz Mohayai <tmohayai@hawk.iit.edu>
2021-01-19 16:47:14 -0600

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   run_nu.sh

--- run_nu.sh
+++ run_nu.sh
@@ -3,6 +3,8 @@
 # Once you downloaded the desired geometry/gdml file, make sure you change the "GEOMETRY" and "TOPVOL" to the appropriate arguments. 
 # GEOMETRY is set to nd_hall_mpd_only, by default. That corresponds to nd_hall_mpd_only.gdml file and is the geometry for only an ND-GAr in the near detector hall.
 # TOPVOL volGasTPC is the gas TPC only portion of ND_GAr.
+
+
 #! /usr/bin/env bash
 
 ####################
@@ -14,6 +16,9 @@ TEST=$4
 
 # GENIE gevgen_fnal app requires the user to input the neutrino running mode; FHC is forward horn current for neutrino running mode and RHC is 
 # reverse horn current for anti-neutrino running mode
+
+# To generate neutrino mode GENIE files, type "source run_nu.sh FHC" on the command line. For anti-neutrino running mode, type "source run_nu.sh RHC", instead.
+
 if [ "${HORN}" != "FHC" ] && [ "${HORN}" != "RHC" ]; then
     echo "Invalid beam mode ${HORN}"
     echo "Must be FHC or RHC"
@@ -47,7 +52,7 @@ RNDSEED=$((${PROCESS}+${FIRST}))
 #NEVENTS="-n ${NPER}"      # No. of events, -e XE16 for POT
 
 #
-GEOMETRY="nd_hall_mpd_only" 
+GEOMETRY="nd_hall_only_mpd" 
 TOPVOL="volTPCGas"
 SPOT=10
 NEVENTS="-n 200"
