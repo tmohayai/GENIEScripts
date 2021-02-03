fce07d3497dc0bbfe0bead842cc122338cbc78ad
Tanaz Mohayai <tmohayai@users.noreply.github.com>
2021-02-03 08:27:26 -0600

    updated comment

--- gst_converter.sh
+++ gst_converter.sh
@@ -1,5 +1,6 @@
 # bash script to convert the ghep format to gst
 # the script would run in a sub-directory within the same directory as the ghep files 
+# change the upper limit of the loop variable c, as needed
 
 for ((c=0; c<=100; c++ ))
 do
